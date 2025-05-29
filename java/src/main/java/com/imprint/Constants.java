fd714b6727eea2f99c4786dfc4563fb21a129962
Brent Johnson <bj144280@alum.villanova.edu>
2025-05-29 17:43:28 -0400

    java - initial commit; mostly types for now

---
+++ java/src/main/java/com/imprint/Constants.java
@@ -0,0 +1,8 @@
+package com.imprint;
+
+public final class Constants {
+    public static final byte MAGIC = 0x49;          // ASCII 'I'
+    public static final byte VERSION = 0x01;
+    
+    private Constants() {} // Utility class
+}
